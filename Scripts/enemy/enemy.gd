f403498621ac84f22b0475af551f389a589b38f2
Lunoro <LennySchurer@gmail.com>
2025-09-30 01:02:38 +0200

    update enemy

--- Scripts/enemy/enemy.gd
+++ Scripts/enemy/enemy.gd
@@ -3,4 +3,5 @@ extends CharacterBody2D
 @onready var vision_area = $Area2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body.name)
+	var distance = body.global_position - global_position;
+	print(distance)
