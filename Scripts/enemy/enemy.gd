628513ed9eec17ae3a525a533d736247ad2e4791
Lenny <lenny@pop-os.tail4e4634.ts.net>
2025-09-30 15:33:38 +0200

    added Assets

--- Scripts/enemy/enemy.gd
+++ Scripts/enemy/enemy.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 @onready var vision_area = $Area2D
+var in_sight = false;
 
 func _process(delta: float) -> void:
 	# rotate NPC 360 degrees in 90 degrees ticks
 	# update area 2d fov 
-	pass
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	var distance = body.global_position - global_position;
-	print(distance)
+	if body.name == "Player":
+		var distance = body.global_position - global_position;
+		print(distance.length());
+		in_sight = true
