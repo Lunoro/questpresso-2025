b70ec0bf315e8220d9988838e775fbb757b4796e
Lunoro <LennySchurer@gmail.com>
2025-09-30 19:01:49 +0200

    added attack animation

--- Scripts/player/movement.gd
+++ Scripts/player/movement.gd
@@ -5,15 +5,20 @@ enum Direction {LEFT, RIGHT, UP, DOWN}
 @export var speed = 125.0
 
 var direction : Direction = Direction.DOWN
-var is_standing = true;
+var is_moving = false;
 var is_attacking = false;
 
 var input_direction
 
 func get_input():
+	if(is_attacking):
+		velocity = Vector2(0, 0);
+		return
+		
 	input_direction = Input.get_vector("left", "right", "up", "down")
+	listen_for_attack()
 	velocity = input_direction * speed
-	is_standing = velocity.x == 0 && velocity.y == 0
+	is_moving = velocity.x != 0 || velocity.y != 0
 
 func _physics_process(delta):
 	get_input()
@@ -24,12 +29,19 @@ func _physics_process(delta):
 func change_animation():
 	var animation_name = "idle_" + Direction.keys()[direction].to_lower()
 	
-	if(is_attacking):
-		animation_name = "attack_" + Direction.keys()[direction].to_lower()
-	if(!is_standing):
+	if(is_moving):
 		animation_name = "move_" + Direction.keys()[direction].to_lower()
 	
+	if(is_attacking):
+		animation_name = "attack_" + Direction.keys()[direction].to_lower()
+	
 	$AnimatedSprite2D.play(animation_name)
+	await $AnimatedSprite2D.animation_finished
+	is_attacking = false
+	
+func listen_for_attack():
+	if Input.is_action_just_released("click"):
+		is_attacking = true
 
 func change_direction(x : int, y : int):
 	if x > 0:
