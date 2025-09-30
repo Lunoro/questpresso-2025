f81b90876d398862e6e046a859a4aac403eb9e54
Lunoro <LennySchurer@gmail.com>
2025-09-30 13:18:17 +0200

    updated enemy

--- Scripts/enemy/enemy.gd
+++ Scripts/enemy/enemy.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @onready var vision_area = $Area2D
 
+func _process(delta: float) -> void:
+	# rotate NPC 360 degrees in 90 degrees ticks
+	# update area 2d fov 
+	pass
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	var distance = body.global_position - global_position;
 	print(distance)
