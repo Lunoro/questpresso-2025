b66deed5187d189b281f03aee4f1c6d7f60138fd
Lenny <lenny@pop-os.tail4e4634.ts.net>
2025-09-30 16:16:03 +0200

    updated enemy ai

--- Scripts/enemy/enemy.gd
+++ Scripts/enemy/enemy.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 
 @onready var vision_area = $Area2D
-var in_sight = false;
+@onready var navigation_agent = $NavigationAgent2D
+@export var target : CharacterBody2D
+@export var speed : int = 200;
 
-func _process(delta: float) -> void:
-	# rotate NPC 360 degrees in 90 degrees ticks
-	# update area 2d fov 
-	
+var in_sight = false
+
+func _physics_process(delta: float) -> void:
+	if in_sight:
+		navigation_agent.target_position = target.global_position
+		velocity = global_position.direction_to(navigation_agent.get_next_path_position()) * speed;
+		move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		var distance = body.global_position - global_position;
-		print(distance.length());
 		in_sight = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		in_sight = false
+		
