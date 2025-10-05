3abdcc38d26cb3af094b0e68fcc95c1374b2e344
LadonFire <leander.plaettner@gmx.de>
2025-10-05 22:55:18 +0200

    added deathscreen

--- Objects/UI/ui.gd
+++ Objects/UI/ui.gd
@@ -5,9 +5,22 @@ extends CanvasLayer
 var text : String
 
 func _ready() -> void:
-	pass
+	$deathscreen.hide()
+	$retry.hide()
+	$win.hide()
+
 
 func _process(delta: float) -> void:
+	if player.is_dead == true: 
+		$Label.hide()
+		$deathscreen.show()
+		$retry.show()
+		print("'s dead")
+		return
+	if $"../Boss".is_dead: 
+		$Label.hide()
+		$win.show()
+		return
 	text += "Health: " + str(round(player.health))
 	if player.regeneration > 0: text += "\nregeneration : " + str(player.regeneration)
 	if player.armor_bonus > 0: text += "\nextra armor: " + str(player.armor_bonus)
@@ -19,3 +32,7 @@ func _process(delta: float) -> void:
 	if player.speed_multiplier < 1: text += "\nslowness" + str(player.speed_multiplier)
 	$Label.text = text.to_upper()
 	text = ""
+
+
+func _on_retry_button_down() -> void:
+	get_tree().reload_current_scene()
