b507746a2bdccb23c2d9f616e4c12d9d0772e307
Lunoro <LennySchurer@gmail.com>
2025-10-05 22:38:30 +0200

    Added stuff to stage1

--- Objects/Portal/portal.gd
+++ Objects/Portal/portal.gd
@@ -5,8 +5,7 @@ extends Area2D
 	# if player in area 
 	# button pressed
 	# player global_postion = marker global_position
-	
 
 func _on_body_entered(body: Node2D) -> void:
-	if(body.name == "Player"):
+	if(body.name == "player"):
 		body.global_position = teleport_point.global_position
--- misc/area_2d2.gd
+++ misc/area_2d2.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://test.tscn")
+	get_tree().change_scene_to_file("res://Scenes/stage2.tscn")
